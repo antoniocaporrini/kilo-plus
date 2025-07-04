0731bd0c99294b1ee11ea0aea17caae7fcf234ae
antoniocaporrini <caporrini@gmail.com>
2025-07-04 11:00:36 +0200

    added explanations to the code

--- kilo.c
+++ kilo.c
@@ -1,41 +1,75 @@
-/*** includes ***/
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
+/*** INCLUDES ***/
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <termios.h>
+#include <time.h>
 #include <unistd.h>
 
-/*** defines ***/
+/*** DEFINES ***/
+// Macros & Constants
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define KILO_VERSION "0.0.1"
+#define KILO_TAB_STOP 8
 
+// Enumerations
+// Used to defined special keys that are not part of the ASCII character set.
 // We will give them a large integer value that is out of the range of a char, so that they don’t conflict with any ordinary keypresses
-enum editorKey {
+enum editorKey { 
   ARROW_LEFT = 1000,
-  ARROW_RIGHT = 'd',
-  ARROW_UP = 'w',
-  ARROW_DOWN = 's'
+  ARROW_RIGHT,
+  ARROW_UP,
   ARROW_DOWN,
   DEL_KEY,
   PAGE_UP,
-  PAGE_DOWN
+  PAGE_DOWN,
   HOME_KEY,
-  END_KEY,
+  END_KEY
 };
 
-/*** data ***/
+/*** DATA ***/
+// Data Structures
+// erow stands for “editor row”
+// We will use a struct to store the state of the editor, including the cursor position, the number of rows, and the rows themselves.
+// It basically represents a single line of text in the editor.
+typedef struct erow { 
+  int size;
+  int rsize;
+  char *chars;
+  char *render;
+} erow;
+
+// Global State
+// It contains the cursor position, the number of rows, the rows themselves, the filename, and the status message.
 struct editorConfig {
   int cx, cy;
+  int rx;
+  int rowoff;
+  int coloff;
   int screenrows;
   int screencols;
+  int numrows;
+  erow *row;
+  char *filename;
+  char statusmsg[80];
+  time_t statusmsg_time;
   struct termios orig_termios;
 };
+
 struct editorConfig E;
 
-/*** terminal ***/
+/*** TERMINAL ***/
+// The die function is used to handle errors in a consistent way.
+// It clears the screen, moves the cursor to the top-left corner, prints an error message, and exits the program.
 void die(const char *s) {
   write(STDOUT_FILENO, "\x1b[2J", 4);
   write(STDOUT_FILENO, "\x1b[H", 3);
@@ -44,11 +78,14 @@ void die(const char *s) {
   exit(1);
 }
 
+// disableRawMode restores the terminal to its original state.
 void disableRawMode() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
     die("tcsetattr"); // We store the original terminal attributes in a global variable, orig_termios
 } // We assign the orig_termios struct to the raw struct in order to make a copy of it before we start making our changes.
 
+// enableRawMode sets the terminal to raw mode, which allows us to read input one character at a time without waiting for a newline.
+// It also disables some terminal features that we don’t need, such as echoing input and interpreting special characters like Ctrl+C.
 void enableRawMode() {
   
   if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
@@ -66,7 +103,9 @@ void enableRawMode() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
-int editorReadKey() { // editorReadKey()’s job is to wait for one keypress, and return it
+// editorReadKey()’s job is to wait for one keypress, and return it
+// It reads from the standard input (STDIN_FILENO) and handles escape sequences for special keys.
+int editorReadKey() { 
   int nread;
   char c;
   while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
@@ -112,6 +151,7 @@ int editorReadKey() { // editorReadKey()’s job is to wait for one keypress, an
   }
 }
 
+// getCursorPosition() sends an escape sequence to the terminal to request the cursor position, and then reads the response.
 int getCursorPosition(int *rows, int *cols) {
   char buf[32];
   unsigned int i = 0;
@@ -128,6 +168,7 @@ int getCursorPosition(int *rows, int *cols) {
   return 0;
 }
 
+// getWindowSize() uses the ioctl system call to get the size of the terminal window.
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
@@ -140,14 +181,86 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
-/*** append buffer ***/
+/*** ROW OPERATIONS ***/
+int editorRowCxToRx(erow *row, int cx) {
+  int rx = 0;
+  int j;
+  for (j = 0; j < cx; j++) {
+    if (row->chars[j] == '\t')
+      rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP);
+    rx++;
+  }
+  return rx;
+}
+
+void editorUpdateRow(erow *row) {
+  int tabs = 0;
+  int j;
+  for (j = 0; j < row->size; j++)
+    if (row->chars[j] == '\t') tabs++;
+  free(row->render);
+  row->render = malloc(row->size + tabs*(KILO_TAB_STOP - 1) + 1);
+  int idx = 0;
+  for (j = 0; j < row->size; j++) {
+    if (row->chars[j] == '\t') {
+      row->render[idx++] = ' ';
+      while (idx % KILO_TAB_STOP != 0) row->render[idx++] = ' ';
+    } else {
+      row->render[idx++] = row->chars[j];
+    }
+  }
+  row->render[idx] = '\0';
+  row->rsize = idx;
+}
+
+void editorAppendRow(char *s, size_t len) {
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  int at = E.numrows;
+  E.row[at].size = len;
+  E.row[at].chars = malloc(len + 1);
+  memcpy(E.row[at].chars, s, len);
+  E.row[at].chars[len] = '\0';
+
+  E.row[at].rsize = 0;
+  E.row[at].render = NULL;
+  editorUpdateRow(&E.row[at]);
+
+  E.numrows++;
+}
+
+
+/*** FILE I/O ***/
+void editorOpen(char *filename) {
+  free(E.filename);
+  E.filename = strdup(filename);
+
+  FILE *fp = fopen(filename, "r");
+  if (!fp) die("fopen");
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen;
+  while ((linelen = getline(&line, &linecap, fp)) != -1) {
+    while (linelen > 0 && (line[linelen - 1] == '\n' ||
+                           line[linelen - 1] == '\r'))
+      linelen--;
+    editorAppendRow(line, linelen);
+  }
+  free(line);
+  fclose(fp);
+}
+
+/*** APPEND BUFFER ***/
+// What is an append buffer?
+// An append buffer is a data structure that allows us to efficiently build a string in memory before writing it to the terminal.
+// It is used to reduce the number of write() system calls, which can be expensive in terms of performance.
+// The abuf struct is used to build a string in memory before writing it to the terminal.
 struct abuf {
   char *b;
   int len;
 };
+#define ABUF_INIT {NULL, 0} 
 
-#define ABUF_INIT {NULL, 0}
-
+// The abAppend function appends a string to the abuf struct.
 void abAppend(struct abuf *ab, const char *s, int len) {
   char *new = realloc(ab->b, ab->len + len);
   if (new == NULL) return;
@@ -159,59 +272,144 @@ void abFree(struct abuf *ab) {
   free(ab->b);
 }
 
-/*** output ***/
-void editorDrawRows(struct abuf *ab) {
-  int y;
-  for (y = 0; y < E.screenrows; y++) {
-    if (y == E.screenrows / 3) {
-      char welcome[80];
-      int welcomelen = snprintf(welcome, sizeof(welcome),
-        "Kilo editor -- version %s", KILO_VERSION);
-      if (welcomelen > E.screencols) welcomelen = E.screencols;
-      int padding = (E.screencols - welcomelen) / 2;
-      if (padding) {
-        abAppend(ab, "~", 1);
-        padding--;
-      }
-      while (padding--) abAppend(ab, " ", 1);
-      abAppend(ab, welcome, welcomelen);
-    } else {
-      abAppend(ab, "~", 1);
-    }
-    abAppend(ab, "\x1b[K", 3);
-    if (y < E.screenrows - 1) {
-      abAppend(ab, "\r\n", 2);
-    }
+/*** OUTPUT ***/
+// editorScroll() updates the rowoff and coloff variables to ensure that the cursor is always visible on the screen.
+void editorScroll() {
+  E.rx = 0;
+  if (E.cy < E.numrows) {
+    E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+  }
+  if (E.cy < E.rowoff) {
+    E.rowoff = E.cy;
+  }
+  if (E.cy >= E.rowoff + E.screenrows) {
+    E.rowoff = E.cy - E.screenrows + 1;
+  }
+  if (E.rx < E.coloff) {
+    E.coloff = E.rx;
+  }
+  if (E.rx >= E.coloff + E.screencols) {
+    E.coloff = E.rx - E.screencols + 1;
   }
 }
 
+// editorDrawRows() draws the rows of the editor to the terminal.
+// It iterates through each row and appends the rendered text to the abuf struct.
+// If there are no rows, it displays a welcome message in the center of the screen.
+void editorDrawRows(struct abuf *ab) {
+  int y;
+  for (y = 0; y < E.screenrows; y++) {
+    int filerow = y + E.rowoff;
+    if (filerow >= E.numrows) {
+      if (E.numrows == 0 && y == E.screenrows / 3) {
+        char welcome[80];
+        int welcomelen = snprintf(welcome, sizeof(welcome),
+          "Kilo editor -- version %s", KILO_VERSION);
+        if (welcomelen > E.screencols) welcomelen = E.screencols;
+        int padding = (E.screencols - welcomelen) / 2;
+        if (padding) {
+          abAppend(ab, "~", 1);
+          padding--;
+        }
+        while (padding--) abAppend(ab, " ", 1);
+        abAppend(ab, welcome, welcomelen);
+      } else {
+        abAppend(ab, "~", 1);
+      }
+    } else {
+      int len = E.row[filerow].rsize - E.coloff;
+      if (len < 0) len = 0;
+      if (len > E.screencols) len = E.screencols;
+      abAppend(ab, &E.row[filerow].render[E.coloff], len);
+    }
+    abAppend(ab, "\x1b[K", 3);
+    abAppend(ab, "\r\n", 2);
+  }
+}
 
+// editorDrawStatusBar() draws the status bar at the bottom of the screen.
+void editorDrawStatusBar(struct abuf *ab) {
+  abAppend(ab, "\x1b[7m", 4);
+  char status[80], rstatus[80];
+  int len = snprintf(status, sizeof(status), "%.20s - %d lines",
+    E.filename ? E.filename : "[No Name]", E.numrows);
+  int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d",
+    E.cy + 1, E.numrows);
+  if (len > E.screencols) len = E.screencols;
+  abAppend(ab, status, len);
+  while (len < E.screencols) {
+    if (E.screencols - len == rlen) {
+      abAppend(ab, rstatus, rlen);
+      break;
+    } else {
+      abAppend(ab, " ", 1);
+      len++;
+    }
+  }
+  abAppend(ab, "\x1b[m", 3);
+  abAppend(ab, "\r\n", 2);
+}
+
+// editorDrawMessageBar() draws the message bar at the bottom of the screen.
+void editorDrawMessageBar(struct abuf *ab) {
+  abAppend(ab, "\x1b[K", 3);
+  int msglen = strlen(E.statusmsg);
+  if (msglen > E.screencols) msglen = E.screencols;
+  if (msglen && time(NULL) - E.statusmsg_time < 5)
+    abAppend(ab, E.statusmsg, msglen);
+}
+
+// editorRefreshScreen() refreshes the entire screen by drawing all the rows, the status bar, and the message bar.
+// It also moves the cursor to the current position of the editor.
 void editorRefreshScreen() {
+  editorScroll();
   struct abuf ab = ABUF_INIT;
   abAppend(&ab, "\x1b[?25l", 6);
   abAppend(&ab, "\x1b[H", 3);
   editorDrawRows(&ab);
+  editorDrawStatusBar(&ab);
+  editorDrawMessageBar(&ab);
+
+
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
+                                            (E.rx - E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
   abAppend(&ab, "\x1b[?25h", 6);
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
 }
 
+// editorSetStatusMessage() sets the status message to be displayed in the status bar.
+void editorSetStatusMessage(const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vsnprintf(E.statusmsg, sizeof(E.statusmsg), fmt, ap);
+  va_end(ap);
+  E.statusmsg_time = time(NULL);
+}
 
-
-/*** input ***/
+/*** INPUT ***/
+// editorMoveCursor() moves the cursor based on the key pressed.
 void editorMoveCursor(int key) {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
+
   switch (key) {
     case ARROW_LEFT:
       if (E.cx != 0) {
         E.cx--;
+      } else if (E.cy > 0) {
+        E.cy--;
+        E.cx = E.row[E.cy].size;
       }
       break;
     case ARROW_RIGHT:
-      if (E.cx != E.screencols - 1) {
+      if (row && E.cx < row->size) {
         E.cx++;
+      } else if (row && E.cx == row->size) {
+        E.cy++;
+        E.cx = 0;
       }
       break;
     case ARROW_UP:
@@ -220,39 +418,53 @@ void editorMoveCursor(int key) {
       }
       break;
     case ARROW_DOWN:
-      if (E.cy != E.screenrows - 1) {
+      if (E.cy < E.numrows) {
         E.cy++;
       }
       break;
   }
-}
+
+  row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if (E.cx > rowlen) {
+    E.cx = rowlen;
+  }
+} 
 
 
-void editorProcessKeypress() { // editorProcessKeypress() waits for a keypress, and then handles it
+// editorProcessKeypress() waits for a keypress, and then handles it
+// It checks for special keys like Ctrl+Q to quit, arrow keys to move the cursor, and Home/End keys to move to the beginning or end of the line.
+void editorProcessKeypress() {
   int c = editorReadKey();
+  printf("KEY PRESSED: %d\r\n", c); 
+
   switch (c) {
     case CTRL_KEY('q'):
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
-
     case HOME_KEY:
       E.cx = 0;
       break;
     case END_KEY:
-      E.cx = E.screencols - 1;
+      if (E.cy < E.numrows)
+        E.cx = E.row[E.cy].size;
       break;
-      
     case PAGE_UP:
     case PAGE_DOWN:
       {
+        if (c == PAGE_UP) {
+          E.cy = E.rowoff;
+        } else if (c == PAGE_DOWN) {
+          E.cy = E.rowoff + E.screenrows - 1;
+          if (E.cy > E.numrows) E.cy = E.numrows;
+        }
         int times = E.screenrows;
         while (times--)
           editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
       }
       break;
-      
     case ARROW_UP:
     case ARROW_DOWN:
     case ARROW_LEFT:
@@ -263,24 +475,38 @@ void editorProcessKeypress() { // editorProcessKeypress() waits for a keypress, 
 }
 
 
-/*** init ***/
+/*** INITIALIZATION ***/
+// initEditor() initializes the editor state, setting the cursor position, row offsets, and screen size.
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
+  E.rx = 0;
+  E.rowoff = 0;
+  E.coloff = 0;
+  E.numrows = 0;
+  E.row = NULL;
+  E.filename = NULL;
+  E.statusmsg[0] = '\0';
+  E.statusmsg_time = 0;
 
-  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize"); 
 }
 
-int main() {
+int main(int argc, char *argv[]) {
   enableRawMode();
-  initEditor(); // initEditor()’s job will be to initialize all the fields in the E struct.
+  initEditor();
+  if (argc >= 2) {
+    editorOpen(argv[1]);
+  }
+
+  editorSetStatusMessage("HELP: Ctrl-Q = quit");
 
 
   while (1) {
     editorRefreshScreen();
     editorProcessKeypress();
   }
-  
   return 0;
 }
 
@@ -288,19 +514,3 @@ int main() {
 
 
 // \x1b represents the escape character
-
-
-
-
-
-
-
-
-// char c = '\0';
-// if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read"); // errno and EAGAIN come from <errno.h>.
-// if (iscntrl(c)) { // comes from <ctype.h> - iscntrl() tests whether a character is a control character
-//   printf("%d\r\n", c); // Control characters are nonprintable characters that we don’t want to print to the screen
-// } else {
-//   printf("%d ('%c')\r\n", c, c);
-// }
-// if (c == CTRL_KEY('q')) break;
